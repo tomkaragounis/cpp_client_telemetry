d7dfaff69b34b6adf633da2f974af24779f95fd7
tomkaragounis <thkarago@microsoft.com>
2022-07-15 15:07:44 -0700

    test with no docker files

--- docker/android-base-image/assemble.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-cd ${GITHUB_WORKSPACE}/lib/android_build
-chmod +x gradlew
-which cmake
-echo $PATH
-./gradlew assemble || exit 1
-tar cvf ${GITHUB_WORKSPACE}/artifacts.tar ariasdk/build/outputs/aar ariasdk/build/intermediates/merged_native_libs
